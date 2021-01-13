9ab51d1678f48fdd734a14dffff18a22b2f42ea2
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-13 15:58:01 -0600

    new changes

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        #text_from_user = params[:words]
+
+        #@piglatintext = PigLatinizer.new(text_from_user)
+
+
+    end
+
 end
